254b9d8a7b72225f0d5a7423664457d378713578
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-06-04 16:43:15 +0300

    start-server

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+//const mongoose = require('mongoose');
+require('dotenv').config();
+
+const server = express();
+server.use(cors());
+//app.use(express.json());
+
+const PORT = process.env.PORT;
+
+//NOW TEST THE URL
+// http://localhost:3001/
+server.get('/', (request, response) => {
+    let str = 'hello from back end';
+    response.status(202).send(str);
+})
+// if we search about something wrong 
+server.get('*', (req, res) => {
+    res.status(404).send('not found');
+})
+
+// step 1 do listening 
+server.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
+})
